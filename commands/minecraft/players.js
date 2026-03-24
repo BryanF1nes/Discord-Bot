842920a22b8e6c64139927715fe57bc19c8bde32
Bryan <bryanfinesw@gmail.com>
2026-03-24 12:36:03 -0400

    feat: added player command

---
+++ commands/minecraft/players.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getServerProcess } = require('../../serverProcess.js');
+require("dotenv").config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('players')
+        .setDescription('Provides a list of players that are online.'),
+    async execute(interaction) {
+
+        if (!getServerProcess()) {
+            return interaction.reply('The server is offline');
+        }
+
+        try {
+            const result = await fetch(`https://api.mcstatus.io/v2/status/java/${process.env.SERVER_IP}`);
+            const data = await result.json();
+
+            const playerList = data.players.list;
+
+            if (playerList.length === 0) {
+                return interaction.reply('There are no players online.');
+            }
+
+            return interaction.reply(`Players: ${playerList.map(player => player.name_clean).join(", ")}`);
+
+        } catch (error) {
+            console.error("Status update failed: ", error);
+        }
+    }
+}
